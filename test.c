5228223f3695192ff8b3bbbb8e8ac593b613e501
Hayward_linux <mhchanax@gmail.com>
2023-09-27 19:45:34 +0800

    test.c

---
+++ test.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h> // For constants that are required in open/read/write/close syscalls
+#include <sys/wait.h> // For wait() - suppress warning messages
+#include <fcntl.h>    // For open/read/write/close syscalls
+#include <signal.h>   // For signal handling
+int main(){
+//Q3.2
+    // if(fork() == 0) {
+    //     printf("1");
+    // }
+    // else {
+    //     if(fork() == 0) {
+    //     }
+    //     else {
+    //         wait(NULL);
+    //         wait(NULL);
+    //     }
+    //     printf("2");
+    // }
+    // printf("3");
+
+//Q3.2
+    int index = 0;
+    for(int i = 0; i < 2; i++){
+        int pid = fork();
+        if(pid != 0){
+        fork();
+        printf("a");
+        }
+        else if(index == 0){
+            fork();
+            index++;
+            printf("b");
+        }
+        else{
+            fork();
+            printf("c");
+        }
+    }
+}
